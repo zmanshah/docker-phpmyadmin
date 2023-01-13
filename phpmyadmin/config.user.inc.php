7c2d63a0f58cca21228c5cb6562ef2488a8d19b0
zmanshah <120363414+zmanshah@users.noreply.github.com>
2023-01-13 09:20:20 +0800

    Add files
    Add file required for phpmyadmin

---
+++ phpmyadmin/config.user.inc.php
@@ -0,0 +1,52 @@
+<?php
+/* Override Servers array */
+$cfg['Servers'] = [
+    1 => [
+        'auth_type' => 'cookie',
+        'host' => 'hostname-or-IP',
+        'port' => 3306,
+        'verbose' => 'Database1',
+    ],
+    6 => [
+        'auth_type' => 'cookie',
+        'host' => 'hostname-or-IP',
+        'port' => 3306,
+        'verbose' => 'Database2',
+    ],
+    2 => [
+        'auth_type' => 'cookie',
+        'host' => 'hostname-or-IP',
+        'port' => 3306,
+        'verbose' => 'Database3',
+    ],
+    3 => [
+        'auth_type' => 'cookie',
+        'host' => 'hostname-or-IP',
+        'port' => 3306,
+        'verbose' => 'Database4',
+    ],
+    4 => [
+        'auth_type' => 'cookie',
+        'host' => 'hostname-or-IP',
+        'port' => 3306,
+        'verbose' => 'Database5',
+    ],
+    5 => [
+        'auth_type' => 'cookie',
+        'host' => 'hostname-or-IP',
+        'port' => 3306,
+        'verbose' => 'Database6',
+    ],
+    6 => [
+        'auth_type' => 'cookie',
+        'host' => 'hostname-or-IP',
+        'port' => 3306,
+        'verbose' => 'Database7',
+    ],
+    7 => [
+        'auth_type' => 'cookie',
+        'host' => 'hostname-or-IP',
+        'port' => 3306,
+        'verbose' => 'Database8',
+    ]
+];
